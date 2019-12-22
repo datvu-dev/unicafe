e49437fb181c5c68447b558cb773b01f18c297d3
Dat Vu <dothanhdat.vu@gmail.com>
2019-12-23 09:46:21 +1100

    Apply counter functionality on the front end

--- src/index.js
+++ src/index.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
-import reducer from './reducer'
+import counterReducer from './reducer'
 
-const store = createStore(reducer)
+const store = createStore(counterReducer)
 
 const App = () => {
   const good = () => {
     store.dispatch({
-      type: 'GOOD'
+      type: 'good'
+    })
+  }
+
+  const neutral = () => {
+    store.dispatch({
+      type: 'ok'
+    })
+  }
+
+  const bad = () => {
+    store.dispatch({
+      type: 'bad'
     })
   }
 
   return (
     <div>
       <button onClick={good}>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <button onClick={neutral}>ok</button>
+      <button onClick={bad}>bad</button>
       <button>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
